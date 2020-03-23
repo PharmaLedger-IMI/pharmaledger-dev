40d3c8e8f969822f48791da980225ab5afddae5c
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-03-23 13:49:54 +0200

    cardinal update

--- scripts/controllers/base-controllers/DefaultFormController.js
+++
@@ -1,8 +0,0 @@
-import BindableController from "./BindableController.js";
-
-export default class DefaultFormController extends BindableController {
-    constructor(element, model) {
-        super(element);
-        this.model = this.setModel(model);
-    }
-}
